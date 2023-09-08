beb6160341f3b05244d29b9dbd79612af2915c1e
Ng Huong Duyen <huongduyen.work@gmail.com>
2023-09-08 10:27:41 +0700

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+namespace FirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+
+            while (true)
+            {
+                Console.Write("Nhập số: ");
+                int x = Convert.ToInt32(Console.ReadLine());
+
+                if (x >= 1 && x <= 100)
+                {
+                    Console.WriteLine("OK. " + x);
+                    break;
+                }
+                Console.WriteLine("Nhập sai.");
+            }
+
+            
+
+        }
+    }
+}
