888376cccb693b342a810c96573214aa99e5116b
Ng Huong Duyen <huongduyen.work@gmail.com>
2023-09-13 08:59:02 +0700

    Unused files

---
+++ Baitap01/01.cs
@@ -0,0 +1,11 @@
+using System.*
+
+namespace Bai01{
+    internal class Program{
+        static void Main(string[] args){
+            int arr = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(arr);
+        }
+    }
+}
