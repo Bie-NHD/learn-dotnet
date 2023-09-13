071f34de27b7b4109388da28ce612b7d11666779
Ng Huong Duyen <huongduyen.work@gmail.com>
2023-09-13 09:06:03 +0700

    Unused files

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,31 +10,20 @@ namespace FirstApp
         {
             Console.OutputEncoding = Encoding.UTF8;
 
-            // Nhập số trong khoảng [1..100]
-            //while (true)
-            //{
-            //    Console.Write("Nhập số: ");
-            //    int x = Convert.ToInt32(Console.ReadLine());
 
-            //    if (x >= 1 && x <= 100)
-            //    {
-            //        Console.WriteLine("OK. " + x);
-            //        break;
-            //    }
-            //    Console.WriteLine("Nhập sai.");
-            //}
-
-            // Tinhs tổng số lẻ 1..50, bỏ qua 13 và 27
-
-            int tong = 0, count = 0;
-            for (int i = 0; i < 50; i+=2)
+            while (true)
             {
-                tong += i; count++;
+                Console.Write("Nhập số: ");
+                int x = Convert.ToInt32(Console.ReadLine());
+
+                if (x >= 1 && x <= 100)
+                {
+                    Console.WriteLine("OK. " + x);
+                    break;
+                }
+                Console.WriteLine("Nhập sai.");
             }
-            tong = tong - 13 - 27;
-            Console.WriteLine("Odd count: " + count);
-            Console.WriteLine("cách 1: " + tong);
-            Console.WriteLine("Cach 2:" + (Math.Pow(50 / 2, 2) - 13 - 17));
+
             
 
         }
