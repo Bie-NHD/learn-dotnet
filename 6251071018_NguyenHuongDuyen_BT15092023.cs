3a8979883c38b0f73eff2472901c38103f246dd6
Ng Huong Duyen <huongduyen.work@gmail.com>
2023-09-18 22:46:42 +0700

    Baitap-15092023-1

---
+++ 6251071018_NguyenHuongDuyen_BT15092023.cs
@@ -0,0 +1,127 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Data;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        int h = 0;
+        while (true)
+        {
+            Console.WriteLine("Nhap chieu cao h:");
+            if (!int.TryParse(Console.ReadLine(), out h)
+                )
+            {
+                Console.WriteLine("Nhap sai cu phap! (h la so nguyen >0).\nHay nhap lai.\n");
+                continue;
+            }
+            break;
+        }
+        Console.WriteLine("Cac hinh ve voi h = " + h);
+
+        Console.WriteLine("\nIncreasing Right Triangle.");
+
+        paintIncreasingRightTriangle(h);
+        Console.WriteLine("\nDecreasing Left Triangle");
+
+        paintDecreasingLeftTriangle(h);
+        Console.WriteLine("\nIncreasing Left Triangle.");
+        paintIncreasingLeftTriangle(h);
+        Console.WriteLine("\nDecreasing Right Triangle.");
+        paintDecreasingRightTriangle(h);
+    }
+    private static void paintIncreasingRightTriangle(int h,char drawSymbol = '*', char blankSymbol=' ')
+
+    {
+        var arr = new char[h, h];
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < h; j++)
+            {
+                if (j <= i)
+                
+                    arr[i, j] = drawSymbol;
+                else arr[i, j] = blankSymbol;
+            }
+        }
+
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < h; j++)
+            {
+                Console.Write(arr[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+    private static void paintDecreasingLeftTriangle(int h, char drawSymbol = '*', char blankSymbol = ' ')
+
+    {
+        var arr = new char[h, h];
+        for (int row = 0; row < h; row++)
+        {
+            for (int col = 0; col < h; col++)
+            {
+                if (row <= col)
+
+                    arr[row, col] = drawSymbol;
+
+                else arr[row, col] = blankSymbol;
+            }
+        }
+
+        for (int row = 0; row < h; row++)
+        {
+            for (int col = 0; col < h; col++)
+            {
+                Console.Write(arr[row, col]);
+            }
+            Console.WriteLine();
+        }
+
+        
+    }
+    private static void paintIncreasingLeftTriangle(int h)
+    {
+        var arr = new char[h, h];
+        for (int row=0; row < h; row++)
+        {
+            for (int col = h-1; col >= 0; col--)
+            {
+                if (col >= h -1 - row)
+                    arr[row, col] = '*';
+                else arr[row, col] = ' ';
+            }
+        }
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < h; j++)
+            {
+                Console.Write(arr[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+    private static void paintDecreasingRightTriangle(int h)
+    {
+        var arr = new char[h, h];
+        for (int row = 0; row < h; row++)
+        {
+            for (int col = h - 1; col >= 0; col--)
+            {
+                if (col <= h - 1 - row)
+                    arr[row, col] = '*';
+                else arr[row, col] = ' ';
+            }
+        }
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < h; j++)
+            {
+                Console.Write(arr[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ learn-params/learn-params/Program.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Reflection.Metadata.Ecma335;
+
+internal class Program
+{
+    static void Main()
+    {
+        int n, x;
+
+        while (true)
+        {
+
+            Console.WriteLine("Nhap  n:");
+            if (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("n khong hop le.");
+                continue;
+            }
+            break;
+        }
+        Console.WriteLine("n: " + n);
+        Console.WriteLine("C1: {0:f2}", (n == 0) ? " chu y: n = 0" : C1(n));
+        Console.WriteLine("C2: {0:f2}", C2(n));
+        Console.WriteLine("C2: {0:f2}", C3(n));
+
+
+        while (true)
+        {
+
+            Console.WriteLine("Nhap  x:");
+            if (!int.TryParse(Console.ReadLine(), out x))
+            {
+                Console.WriteLine("n khong hop le.");
+                continue;
+            }
+            break;
+        }
+        Console.WriteLine("C4: {0:f2}", C4(x, n));
+
+
+    }
+
+    static int Sum(params int[] numbrs)
+    {
+        return numbrs.Sum(x => x);
+    }
+    static float Sum(params float[] numbrs)
+    {
+        return numbrs.Sum(x => x);
+    }
+    static int SumContinousN(int n)
+    {
+
+        return n * (n + 1) / 2;
+    }
+    static double C1(int n)
+    {
+
+        return (n == 0) ? 0 : Enumerable.Range(1, n).Sum(x => 1.0 / x);
+
+    }
+    static double C2(int n)
+    {
+        if (n == 0) return 1;
+
+        double result = 0;
+
+        //result = Enumerable.Range(0, 2*n+1).Sum(x => 1.0 / (x+=2));
+
+        for (int i = 0; i <= n * 2 + 1; i += 2)
+        {
+            result += 1.0 / i;
+        }
+
+        return result;
+    }
+    static double C3(int n)
+    {
+
+
+        return (n == 0) ? 0 : Enumerable.Range(1, n).Sum(i => 1.0 / SumContinousN(i));
+    }
+
+    public static double C4(int x, int n)
+    {
+        return (n == 0) ? 0 : Enumerable.Range(1, n).Sum(i => Math.Pow(x, i) / SumContinousN(i));
+    }
+}
